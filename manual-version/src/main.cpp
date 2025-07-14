ddd1ebb83007c3c7e01e207dcd946576e447d759
Matt Musiel <80542428+mmusiel@users.noreply.github.com>
2025-07-14 11:00:34 -0500

    Makefile configuration and initial main.cpp setup

---
+++ manual-version/src/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdint>
+#include <iostream>
+
+using BitType = uint32_t;	// Type alias, can change Bit size later
+
+int main()
+{
+	std::cout << "Hello from Mars!\n";
+
+	return 0;
+}
---
+++ stdlib-version/src/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <bitset>
+#include <iostream>
+
+using BitType = uint32_t;	// Type alias, can change Bit size later
+
+int main()
+{
+	std::cout << "Hello from Mars!\n";
+
+	return 0;
+}
