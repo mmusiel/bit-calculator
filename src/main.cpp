3f18a33202427dc98740481dca27a4814164adae
Matt Musiel <80542428+mmusiel@users.noreply.github.com>
2025-07-15 12:05:25 -0500

    Implement IO into main

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@ int main()
 		std::cout << "1) Bitwise operation (AND, OR, XOR, NOT, or Left/Right Shifts)\n";
 		std::cout << "2) Number conversion (Decimal, Binary, Hexadecimal)\n";
 		std::cout << "3) Exit\n";
+
+		constexpr int minMenuOption{ 1 };
+		constexpr int maxMenuOption{ 3 };
+
+		const int menuChoice{ getInt(minMenuOption, maxMenuOption, "Enter menu choice: ") };
+
+		std::cout << menuChoice << '\n';
 		
 	}
 	while(false);
