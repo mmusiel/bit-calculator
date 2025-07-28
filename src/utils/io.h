05b974e88868112545501d527497a36e507fcb51
Matt Musiel <80542428+mmusiel@users.noreply.github.com>
2025-07-28 16:27:33 -0500

    Update io header file

--- src/utils/io.h
+++ src/utils/io.h
@@ -7,14 +7,15 @@
 
 int getMenuChoice(int min, int max, std::string_view prompt);
 char getOperator();
-BitType checkBitTypeRange(std::string_view input);
-BitType validateDecimal(std::string_view input);
+BitType checkBitTypeRange(std::string_view input, int numBase);
 BitType validateHex(std::string_view input);
 BitType validateBinary(std::string_view input);
+BitType validateDecimal(std::string_view input);
 NumberInput getNumberInputWithBase(const std::string& prompt = "Enter positive decimal, hex (prefix 0x), or binary (prefix 0b): ");
 void printBit(BitType number, BitType power);
 void printBinaryMulitpleLines(BitType number);
 void printBinaryOneLine(BitType number);
+void printFormattedNumber(BitType num, NumberBase base);
 void printBitwiseResult(NumberInput num1, char op, NumberInput num2, BitType result);
 
 #endif
